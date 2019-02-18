32cd0e4b13351b5b5c2765c62a26f1a23ee35750
Seo-young Silvia Kim <sskim@caltech.edu>
2019-02-17 16:40:19 -0800

    Add description for oc_colclasses

---
+++ R/oc_colclasses.R
@@ -0,0 +1,7 @@
+#' Column Classes for OCROV Data
+#'
+#' A dataset containing the list of column classes for data from
+#' Orange County Registrar of Voters.
+#'
+#' @format A list of length 62.
+"oc_colclasses"
