a7079cec3cc9d211af02a8cbbdd693b72ba35988
Seo-young Silvia Kim <sskim@caltech.edu>
2019-06-17 17:26:31 -0700

    Accidentally not staged HEAD~2

--- R/changes_report.R
+++ R/changes_report.R
@@ -5,12 +5,16 @@
 #'
 #' @importFrom dplyr "%>%"
 #' @importFrom dplyr mutate
+#' @importFrom purrr map
+#' @importFrom purrr set_names
 #'
 #' @param changes Named list outputed from change_extract.
 #' @param varnames Variables to track changes.
 #' Defaults to NULL.
 #' @param nrow Name of list element which will contain the number of rows
 #' of the input list dataframes.
+#' @param group Grouping variable, such as jurisdiction,
+#' to differentiate changes in variables by the group.
 #'
 #' @return Dataframe with changes' summary statistics.
 #'
@@ -18,40 +22,79 @@
 
 changes_report <- function(changes,
                            varnames = NULL,
-                           nrow = "nrow") {
-  change_prop <- change_n <- totalA <- totalB <- NULL
-  out <- data.frame(
-    labels = c(
-      c("Exact Matches", "Changed", "Dropped", "Added"), varnames
-    ),
-    totalA = rep(
-      sum(
+                           nrow = "nrow",
+                           group = NULL) {
+  . <- .x <- change_prop <- change_n <- totalA <- totalB <- NULL
+  if (is.null(group)) {
+    out <- data.frame(
+      labels = c(
+        c("Exact Matches", "Changed", "Dropped", "Added"), varnames
+      ),
+      totalA = rep(
+        sum(
+          changes[[nrow]][["exact_match"]],
+          changes[[nrow]][["changed"]],
+          changes[[nrow]][["only_A"]]
+        ),
+        length(varnames) + 4
+      ),
+      totalB = rep(
+        sum(
+          changes[[nrow]][["exact_match"]],
+          changes[[nrow]][["changed"]],
+          changes[[nrow]][["only_B"]]
+        ),
+        length(varnames) + 4
+      ),
+      change_n = c(
         changes[[nrow]][["exact_match"]],
         changes[[nrow]][["changed"]],
-        changes[[nrow]][["only_A"]]
-      ),
-      length(varnames) + 4
-    ),
-    totalB = rep(
-      sum(
-        changes[[nrow]][["exact_match"]],
-        changes[[nrow]][["changed"]],
-        changes[[nrow]][["only_B"]]
-      ),
-      length(varnames) + 4
-    ),
-    change_n = c(
-      changes[[nrow]][["exact_match"]],
-      changes[[nrow]][["changed"]],
-      changes[[nrow]][["only_A"]],
-      changes[[nrow]][["only_B"]],
-      unlist(
-        lapply(varnames, function(v) nrow(changes[[paste(v, "A", sep = "_")]]))
+        changes[[nrow]][["only_A"]],
+        changes[[nrow]][["only_B"]],
+        varnames %>%
+          map(nrow(changes[[paste(.x, "A", sep = "_")]])) %>%
+          unlist()
       )
-    )
-  ) %>%
-    mutate(
-      change_prop = change_n / totalB
-    )
+    ) %>%
+      mutate(change_prop = change_n / totalB)
+  } else {
+    names(out[[nrow]]) %>%
+      set_names(., .) %>%
+      map(
+        ~ data.frame(
+          labels = c(
+            c("Exact Matches", "Changed", "Dropped", "Added"), varnames
+          ),
+          group = .x,
+          totalA = rep(
+            sum(
+              changes[[nrow]][[.x]][["exact_match"]],
+              changes[[nrow]][[.x]][["changed"]],
+              changes[[nrow]][[.x]][["only_A"]]
+            ),
+            length(varnames) + 4
+          ),
+          totalB = rep(
+            sum(
+              changes[[nrow]][[.x]][["exact_match"]],
+              changes[[nrow]][[.x]][["changed"]],
+              changes[[nrow]][[.x]][["only_B"]]
+            ),
+            length(varnames) + 4
+          ),
+          change_n = c(
+            changes[[nrow]][[.x]][["exact_match"]],
+            changes[[nrow]][[.x]][["changed"]],
+            changes[[nrow]][[.x]][["only_A"]],
+            changes[[nrow]][[.x]][["only_B"]],
+            varnames %>%
+              map(nrow(changes[[paste(.x, "A", sep = "_")]])) %>%
+              unlist()
+          )
+        ) %>%
+          mutate(change_prop = change_n / totalB)
+      ) %>%
+      bind_rows()
+  }
   return(out)
 }
